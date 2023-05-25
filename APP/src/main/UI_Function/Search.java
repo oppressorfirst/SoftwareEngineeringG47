b21debbfe1bb520b66a9ffd9905e944dc7ff1670
sizhongxu <sizhngxu@bupt.edu.cn>
2023-05-25 21:38:00 +0800

    javadoc update

--- APP/src/main/UI_Function/Search.java
+++ APP/src/main/UI_Function/Search.java
@@ -1,9 +1,25 @@
 package UI_Function;
 
+/**
+ * Title:Search.java
+ * Description: Core function for performing keyword searches on a 2D data array.
+ * Copyright: Copyright (c) 2023/5/25
+ * @author Zhongxu Si
+ * @version 5.0
+ */
 public class Search {
     protected static int object_number = 0;
     protected static int row_number = 0;
 
+    /**
+     * Searches for rows in a 2D data array that contain a specific keyword.
+     *
+     * @param data_array The 2D array to search.
+     * @param keyword The keyword to search for.
+     * @param line_number The number of lines in the data array.
+     * @param row_number The number of rows in the data array.
+     * @return A new 2D array containing rows that match the keyword.
+     */
     public static String[][] keyword_search(String[][] data_array, String keyword, int line_number, int row_number) {
         String[][] object_data;
         // 先遍历数组找到所有目标对象
